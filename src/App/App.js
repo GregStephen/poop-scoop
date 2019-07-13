4aa192fc15287ca476e0c63c8c2d0bd364553731
Greg Stephen <GregAStephen@gmail.com>
2019-07-13 15:35:51 -0500

    home page shows and simple input and console error

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Home from '../components/Home/Home';
+
 import './App.scss';
 import fbConnect from '../helpers/data/fbConnection';
 
@@ -8,6 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Home/>
         <button className="btn btn-success">POOPSCOOOOOP</button>
       </div>
     );
