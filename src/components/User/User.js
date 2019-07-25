14dc086b6415b8b940b4662712cafbf58ccef3c8
Greg Stephen <GregAStephen@gmail.com>
2019-07-25 11:09:56 -0500

    avatar edited

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import $ from 'jquery';
 import {
-  Modal, ModalHeader,
+  Modal, ModalHeader, Button, Collapse, Card, CardBody,
 } from 'reactstrap';
 
 import SingleReview from '../SingleReview/SingleReview';
 import EditUserModalForm from '../EditUserModalForm/EditUserModalForm';
+
+import avatarData from '../../helpers/data/avatarData';
 import userData from '../../helpers/data/userData';
 import reviewData from '../../helpers/data/ratingData';
 import restroomTypeData from '../../helpers/data/restroomTypeData';
@@ -19,6 +22,30 @@ class User extends React.Component {
     reviews: [],
     restroomTypes: [],
     editUserModal: false,
+    collapse: false,
+    status: 'Closed',
+    avatars: [],
+    avatarChoice: '',
+  }
+
+  onEntering = () => {
+    this.setState({ status: 'Opening...' });
+  }
+
+  onEntered = () => {
+    this.setState({ status: 'Opened' });
+  }
+
+  onExiting = () => {
+    this.setState({ status: 'Closing...' });
+  }
+
+  onExited = () => {
+    this.setState({ status: 'Closed' });
+  }
+
+  toggle = () => {
+    this.setState(state => ({ collapse: !state.collapse }));
   }
 
   loadPage = () => {
@@ -26,6 +53,7 @@ class User extends React.Component {
     userData.getUserById(userId)
       .then((userPromise) => {
         this.setState({ user: userPromise.data });
+        this.setState({ avatarChoice: this.state.user.imageUrl });
         reviewData.getRatingByUserId(this.state.user.uid)
           .then((reviewArray) => {
             this.setState({ reviews: reviewArray });
@@ -35,6 +63,9 @@ class User extends React.Component {
     restroomTypeData.getRestroomType()
       .then(restroomTypes => this.setState({ restroomTypes }))
       .catch();
+    avatarData.getAvatars()
+      .then(avatars => this.setState({ avatars }))
+      .catch(err => console.error('trouble getting avatars', err));
   }
 
   componentDidMount() {
@@ -65,6 +96,40 @@ class User extends React.Component {
       .catch(err => console.error('can not delete user', err));
   }
 
+  selectAvatar = (e) => {
+    e.preventDefault();
+    const avatarSelection = $('.avatar-image');
+    for (let i = 0; i < avatarSelection.length; i += 1) {
+      avatarSelection[i].classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+    let tempAvatar = this.state.avatarChoice;
+    tempAvatar = e.target.src;
+    this.setState({ avatarChoice: tempAvatar });
+  }
+
+  createAvatarSelection = () => {
+    const { avatars } = this.state;
+    const avatarSelection = [];
+    Object.keys(avatars).forEach((key, index) => {
+      avatarSelection.push(<div key={key}className="avatar col-3 mb-4">
+      <button><img id='imageUrl' className={ index === 0 ? 'avatar-image selected' : 'avatar-image'} src={avatars[key]} alt={key} onClick={this.selectAvatar}></img></button>
+      </div>);
+    });
+    return avatarSelection;
+  };
+
+  saveNewAvatar = () => {
+    const userId = this.props.match.params.id;
+    const { avatarChoice } = this.state;
+    userData.changeAvatar(avatarChoice, userId)
+      .then(() => {
+        this.loadPage();
+        this.toggle();
+      })
+      .catch();
+  }
+
   render() {
     const { user, reviews, restroomTypes } = this.state;
     const displayReviews = reviews.map(review => (
@@ -88,6 +153,28 @@ class User extends React.Component {
           {firebase.auth().currentUser.uid === user.uid ? <button className="btn btn-danger" onClick={this.deleteProfile}>Delete Profile</button> : ''}
         </div>
         <div className="col-12">
+        {firebase.auth().currentUser.uid === user.uid
+          ? <div className="col-12 row justify-content-start">
+        <Button className="col-auto offset-1" color="primary" onClick={this.toggle}>Edit Avatar</Button>
+        <Collapse
+          isOpen={this.state.collapse}
+          onEntering={this.onEntering}
+          onEntered={this.onEntered}
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+        >
+          <div className="row justify-content-center">
+          <Card className="col-10">
+            <CardBody className="col-12 row">
+            { this.createAvatarSelection()}
+            <Button className="btn btn-info" onClick={this.saveNewAvatar}>Save New Avatar</Button>
+            </CardBody>
+          </Card>
+          </div>
+        </Collapse>
+      </div> : ''}
+      </div>
+        <div className="col-12">
           <h1>Ratings</h1>
           <div className="row justify-content-center">
             { displayReviews }
@@ -95,7 +182,7 @@ class User extends React.Component {
         </div>
         <div>
           <Modal isOpen={this.state.editUserModal} toggle={this.toggleModal}>
-          <ModalHeader toggle={this.toggle}>Edit Yo Self!</ModalHeader>
+          <ModalHeader toggle={this.toggleUserEdit}>Edit Yo Self!</ModalHeader>
             <EditUserModalForm
             toggleUserEdit={this.toggleUserEdit}
             user={user}
